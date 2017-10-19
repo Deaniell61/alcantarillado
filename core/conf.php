b2efaff9356af584ba25e0be64ffa8ecef89276c
Daniel Rodriguez <dannyjose1112@hotmail.com>
2017-10-19 12:02:00 -0600

    [INITIAL]

---
+++ core/conf.php
@@ -0,0 +1,29 @@
+<?php
+// define('HOST','localhost');
+// define('USER','root');
+// define('PASS','1234');
+// define('DB','softronic');
+
+
+define('HOST','localhost');
+define('USER','u328821419_sanan');
+define('PASS','DTUaHUV5ayaAM');
+define('DB','u328821419_sanan');
+
+/*
+define('HOST','mysql.hostinger.mx');
+define('USER','u757044153_dev');
+define('PASS','Softronic16');
+define('DB','u757044153_dev');
+/*
+define('HOST','mysql.hostinger.mx');
+define('USER','u757044153_her');
+define('PASS','uA`JZJ*w2AV:[G');
+define('DB','u757044153_her');
+/*
+define('HOST','mysql.hostinger.mx');
+define('USER','u757044153_demo');
+define('PASS','T78PxQIAszu');
+define('DB','u757044153_demo');
+*/
+?>
